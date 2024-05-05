c2ee759b197ad14a7b2be937311fcd8d4bd5851c
isracamvaz <139937343+isracamvaz@users.noreply.github.com>
2024-05-05 17:51:12 -0600

    Añadiendo proyecto

---
+++ webplayground/messenger/models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import m2m_changed
+
+# Create your models here.
+class Message(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['created']
+
+class ThreadManager(models.Manager):
+    def find(self, user1, user2):
+        queryset = self.filter(users=user1).filter(users=user2)  #Thread.objects.all
+        if len(queryset) > 0:
+            return queryset[0]
+        return None
+    
+    def find_or_create(self, user1, user2):
+        thread = self.find(user1, user2)
+        if thread is None:
+            thread = Thread.objects.create()
+            thread.users.add(user1, user2)
+        return thread
+
+class Thread(models.Model):
+    users = models.ManyToManyField(User, related_name='threads')
+    messages = models.ManyToManyField(Message)
+    updated = models.DateTimeField(auto_now=True)
+
+    objects = ThreadManager()
+
+    class Meta:
+        ordering = ['-updated']
+
+def messages_changed(sender, **kwargs):
+    instance = kwargs.pop("instance", None)
+    action = kwargs.pop("action", None)
+    pk_set = kwargs.pop("pk_set", None)
+    print(instance, action, pk_set)
+
+    false_pk_set = set()
+    if action is "pre_add":
+        for msg_pk in pk_set:
+            msg = Message.objects.get(pk=msg_pk)
+            if msg.user not in instance.users.all():
+                print(f"ups, {msg.user} no forma parte del hilo")
+                false_pk_set.add(msg_pk)
+
+    # Se buscarian los mensajes de flase_pk_set que si estan en pk_set y los borramos
+    pk_set.difference_update(false_pk_set)
+
+    #forzar la actualizacion hacendo save
+    instance.save()
+
+m2m_changed.connect(messages_changed, sender=Thread.messages.through)
